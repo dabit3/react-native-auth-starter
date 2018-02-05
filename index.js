54a6d71fbbc9569801453eb28735e085b048126c
Dabit <dabit@8c85901b6e1c.ant.amazon.com>
2018-02-05 13:59:43 -0800

    added light comment

--- index.js
+++ index.js
@@ -2,17 +2,19 @@ import React from 'react'
 import { AppRegistry } from 'react-native';
 import App from './src/App';
 
+// redux
 import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import rootReducer from './src/reducers'
 import thunk from 'redux-thunk'
-import config from './src/aws-exports'
-
 const store = createStore(rootReducer, applyMiddleware(thunk))
 
+// Amplify
+import config from './src/aws-exports'
 import Amplify from 'aws-amplify'
 Amplify.configure(config);
 
+// App
 const ReduxApp = () => (
   <Provider store={store}>
     <App />
